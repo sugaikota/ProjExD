321c46965dcf02c1cf17c9392d818e5bd39e4136
sugaikota <c0b2108747@edu.teu.ac.jp>
2022-10-18 13:13:36 +0900

    3

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,8 +3,15 @@ import tkinter as tk
 
 #1.ゲーム用のウィンドウを生成する.
 #2. 幅 : 1500, 高さ : 900, 背景色 : blackのCanvasを生成する.
-root = tk.Tk()
-root.title("迷路")
-root.geometry("1500x900")
+canvas = tk.Tk()
+canvas.title("迷路")
+canvas.geometry("1500x900")
 
-root.mainloop()
+#3.figフォルダ内の好きなこうかとんのインスタンスを生成し、Canvas内における横 : 300, 縦400の座標に表示させる
+#x座標をcx, y座標をcyとする
+cx, cy = 300, 400
+image_file = "fig/1.png"
+chr = tk.PhotoImage(file = image_file)
+canvas.create_image(cx, cy, image = chr, tag = "chr")
+
+canvas.mainloop()

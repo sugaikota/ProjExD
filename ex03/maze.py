be5fb4666cba35084d22b337dbc4caf08849de46
sugaikota <c0b2108747@edu.teu.ac.jp>
2022-10-18 13:12:13 +0900

    1, 2

---
+++ ex03/maze.py
@@ -0,0 +1,10 @@
+import maze_maker as mk
+import tkinter as tk
+
+#1.ゲーム用のウィンドウを生成する.
+#2. 幅 : 1500, 高さ : 900, 背景色 : blackのCanvasを生成する.
+root = tk.Tk()
+root.title("迷路")
+root.geometry("1500x900")
+
+root.mainloop()
